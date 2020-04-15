d342a23d10d332a2518594580a524e9068954d79
Francesc Quiñones <francescq@gmail.com>
2020-04-15 11:20:47 +0200

    aded list ingredients feature

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -77,7 +77,7 @@ class PrivateTagsApiTests(TestCase):
 
     def test_create_tag_invalid(self):
         """invaoid payload"""
-        payload = {'name':''}
+        payload = {'name': ''}
         res = self.client.post(TAGS_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
